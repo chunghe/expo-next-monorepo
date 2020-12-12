49d4e645ebb1a69d2d1c1f55a5e17b9e8c529712
chunghe <chunghe@gmail.com>
2020-12-12 18:06:26 +0800

    remove Btc

--- packages/components/index.tsx
+++ packages/components/index.tsx
@@ -5,4 +5,3 @@ export {default as Profile} from './Profile';
 export {default as Link} from './Link';
 export {default as CoinDetail} from './CoinDetail';
 export {default as CoinDetailScreen} from './CoinDetailScreen';
-export {default as Btc} from './Btc';
